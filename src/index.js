56240dcc1833c94c5c9063bbd21b0d84e27ffb89
mnaatjes <michael.naatjes87@gmail.com>
2024-11-24 15:08:02 -0500

    created canvas branch

--- src/index.js
+++ src/index.js
@@ -11,3 +11,11 @@ const viewport = document.getElementById('viewport');
  * @name canvas
  */
 const canvas = viewport.querySelector('canvas');
+/**
+ * @ctx
+ */
+const ctx = canvas.getContext('2d');
+/**
+ * debugging
+ */
+console.log(ctx);
