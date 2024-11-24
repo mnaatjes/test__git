8e70f20c8d911c4fde5b6e82a082e5a3b1b7533b
mnaatjes <michael.naatjes87@gmail.com>
2024-11-24 15:12:54 -0500

    set canvas width

--- src/index.js
+++ src/index.js
@@ -6,15 +6,23 @@
 /**
  * @name viewport
  */
-const viewport = document.getElementById('viewport');
+const viewport      = document.getElementById('viewport');
+viewport.position   = viewport.getBoundingClientRect();
 /**
  * @name canvas
  */
-const canvas = viewport.querySelector('canvas');
+const canvas    = viewport.querySelector('canvas');
+canvas.width    = viewport.position.width - 24;
+canvas.height   = viewport.position.height - 24;
+console.log(canvas);
 /**
  * @ctx
  */
 const ctx = canvas.getContext('2d');
+/**
+ * draw ellipse
+ */
+
 /**
  * debugging
  */
